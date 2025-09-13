c7cb8cf98b92e58c305e1c6068121296f66a52a3
abdoshokrey68 <abdoshokrey68@gmail.com>
2025-09-13 19:36:57 +0300

    latest change with out check && before handle fetch the data from url

--- pdf/generate.js
+++ pdf/generate.js
@@ -1,21 +1,54 @@
 // pdf/generate.js
 
+// const puppeteer = require("puppeteer");
+
+// async function generatePDF(htmlContent) {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   // ضع الـ HTML في الصفحة
+//   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+
+//   // مسار ملف PDF المؤقت
+//   const pdfPath = path.join(__dirname, "output.pdf");
+
+//   // أنشئ الـ PDF
+//   await page.pdf({ path: pdfPath, format: "A4" });
+
+//   await browser.close();
+//   return pdfPath;
+// }
+
+// module.exports = generatePDF;
+
 const puppeteer = require("puppeteer");
 const path = require("path");
-const fs = require("fs");
 
 async function generatePDF(htmlContent) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+  });
 
-  // ضع الـ HTML في الصفحة
+  const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
 
-  // مسار ملف PDF المؤقت
   const pdfPath = path.join(__dirname, "output.pdf");
+  await page.pdf({
+    path: pdfPath,
+    width: "80mm",
+    height: "200mm",
+    printBackground: true,
+    margin: {
+      top: "0mm",
+      right: "0mm",
+      bottom: "0mm",
+      left: "0mm",
+    },
+  });
 
-  // أنشئ الـ PDF
-  await page.pdf({ path: pdfPath, format: "A4" });
+  // await page.pdf({ path: pdfPath, format: "A4" });
 
   await browser.close();
   return pdfPath;
